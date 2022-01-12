f440fb0aaf2aa60a9eebec0ab239115501e1f17f
sanocki <sanockimike@gmail.com>
2022-01-12 15:30:42 -0500

    Added Top Tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 // Local
+import './screens/tabs_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
       // home: CategoriesScreen(),
       initialRoute: '/',
       routes: {
-        '/': (ctx) => const CategoriesScreen(),
+        '/': (ctx) => const TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,24 +9,19 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeal'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate:
-            // The width of the objects in the grid
-            const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200,
-                childAspectRatio: 3 / 2,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20),
-        children: DUMMY_CATEGORIES
-            .map((catData) =>
-                CategoryItem(catData.id, catData.title, catData.color))
-            .toList(),
-      ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate:
+          // The width of the objects in the grid
+          const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+      children: DUMMY_CATEGORIES
+          .map((catData) =>
+              CategoryItem(catData.id, catData.title, catData.color))
+          .toList(),
     );
   }
 }
